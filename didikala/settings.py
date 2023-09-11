36bc4e9a4863721bf3016de8694d3610ab199c79
nima <54316106+nimadorostkar@users.noreply.github.com>
2023-09-11 11:19:22 +0330

    Update settings.py

--- didikala/settings.py
+++ didikala/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -127,7 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 
 # Internationalization
